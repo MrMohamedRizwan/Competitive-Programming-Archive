c2045f1721e39ee29fe25d8aa56d585cb1fc063a
MrMohamedRizwan <109430288+MrMohamedRizwan@users.noreply.github.com>
2024-06-25 22:50:29 +0530

    python basics

---
+++ python/lambda.py
@@ -0,0 +1,21 @@
+# z=lambda x:3*x+1
+# print(z(2))
+
+# full_name=lambda fn,gn:fn.strip().title()+" "+gn.strip().title()
+# print(full_name("  hi","j  "))
+
+
+# def function(a,b,c):
+#     return lambda x:a+b+c+x
+# f=function(1,2,3)(1)
+# print(f)
+
+# arr=['hi a','ab','cd']
+# arr.sort(key=lambda name:name.split(" ")[-1])
+# print(arr)
+def make(x):
+    return 1
+
+arr=[10,20,30]
+# y=list(map(make,arr))
+print(arr.count(10))
